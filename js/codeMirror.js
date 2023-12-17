0c100df1a82f9e815777c83d72f22f57cef5b91b
iLigh2d <lightpavelll@mail.ru>
2023-12-17 23:28:46 +0300

    first commit

---
+++ js/codeMirror.js
@@ -0,0 +1,12 @@
+var yourCodeEditor = CodeMirror.fromTextArea(document.getElementById("yourCode"), {
+    lineNumbers: true,
+    mode: "text/x-java",
+    theme: "dracula"
+});
+
+var testCodeEditor = CodeMirror.fromTextArea(document.getElementById("testCode"), {
+    lineNumbers: true,
+    mode: "text/x-java",
+    theme: "dracula"
+});
+
